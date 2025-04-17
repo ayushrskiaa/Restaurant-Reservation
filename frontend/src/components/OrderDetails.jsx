d2a8bfbf89435075afdb5ca2aea459d7f918155c
ayushrskiaa <ayushkumar2263@iiitmanipur.ac.in>
2025-04-17 17:00:30 +0530

    update

--- frontend/src/components/OrderDetails.jsx
+++ frontend/src/components/OrderDetails.jsx
@@ -9,6 +9,10 @@ const OrderDetails = ({ toggleOrderDetails }) => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [showPhoneInput, setShowPhoneInput] = useState(true);
 
+
+
+
+  
   // Use environment variables for the base URL
   const BASE_URL =
     window.location.hostname === "localhost"
